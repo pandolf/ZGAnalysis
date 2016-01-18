f611d33d6cd38d5416ee93cafb4df32a4c8e933f
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-01-18 14:23:26 +0100

    added some plots

--- analysis/drawZG.cpp
+++ analysis/drawZG.cpp
@@ -69,7 +69,16 @@ int main( int argc, char* argv[] ) {
 
   dt.set_lumi( cfg.lumi() );
 
-  dt.drawPlot( "mZg"  , "boss_mass", "", 50, 200., 1200., "M(Z#gamma)", "GeV" );
+  dt.drawPlot( "nVert"     , "nVert"     , "", 35, 0 , 35, "Number of Vertexes" );
+  dt.drawPlot( "leptType"     , "leptType"     , "", 5, 9.5 , 14.4, "Lepton PDG ID" );
+
+  dt.drawPlot( "mZg"  , "boss_mass", "", 100, 200., 1200., "M(Z#gamma)", "GeV" );
+  dt.drawPlot( "mZg_all"     , "boss_mass", "", 120, 0., 1200., "M(Z#gamma)", "GeV" );
+
+  dt.drawPlot( "mZg_lowptZ"  , "boss_mass", "z_pt<30.", 50, 0., 500., "M(Z#gamma)", "GeV" );
+  dt.drawPlot( "mZ_lowptZ"  , "z_mass", "z_pt<30.", 50, 0., 500., "M(Z)", "GeV" );
+  dt.drawPlot( "ptGamma_lowptZ"  , "gamma_pt", "z_pt<30.", 50, 0., 500., "Photon p_{T}", "GeV" );
+  dt.drawPlot( "ptgOmZg_lowptZ"  , "gamma_pt/boss_mass", "z_pt<30.", 50, 0., 2., "", "" );
 
   dt.drawPlot( "mZ"   , "z_mass", ""            , 50, 50., 150., "M(l^{+}l^{-})", "GeV" );
   dt.drawPlot( "mZee" , "z_mass", "leptType==11", 50, 50., 150., "M(e^{+}e^{-})", "GeV" );
@@ -77,14 +86,19 @@ int main( int argc, char* argv[] ) {
 
   dt.drawPlot( "ptZ"        , "z_pt"    , ""              , 60, 0. , 300., "Z p_{T}"      , "GeV" );
   dt.drawPlot( "ptZ_metCut" , "z_pt"    , "met<50."       , 60, 0. , 300., "Z p_{T}"      , "GeV" );
-  dt.drawPlot( "ptZ_bossCut", "z_pt"    , "boss_mass>200.", 60, 0. , 300., "Z p_{T}"      , "GeV" );
+  dt.drawPlot( "ptZ_bossCut", "z_pt"    , "boss_mass>150. && boss_mass<500.", 60, 0. , 300., "Z p_{T}"      , "GeV" );
 
   dt.drawPlot( "ptGamma" , "gamma_pt", "", 60, 40., 340., "Photon p_{T}" , "GeV" );
+  dt.drawPlot( "etaGamma" , "gamma_eta", "", 50, -3., 3., "Photon #eta" );
 
-  dt.drawPlot( "ptLept0" , "lept0_pt", "", 60, 25., 325., "Leading Lepton p_{T}" , "GeV" );
-  dt.drawPlot( "ptLept1" , "lept1_pt", "", 30, 20., 170., "Trailing Lepton p_{T}" , "GeV" );
+  dt.drawPlot( "ptLept0" , "lept0_pt" , "", 60, 25., 325., "Leading Lepton p_{T}" , "GeV" );
+  dt.drawPlot( "etaLept0", "lept0_eta", "", 50, -3.,   3., "Leading Lepton #eta" );
+
+  dt.drawPlot( "ptLept1"  , "lept1_pt" , "", 30, 20., 170., "Trailing Lepton p_{T}" , "GeV" );
+  dt.drawPlot( "etaLept1" , "lept1_eta", "", 50, -3.,   3., "Trailing Lepton #eta" );
 
   dt.drawPlot( "met"     , "met"     , "", 60, 0. , 300., "Missing E_{T}", "GeV" );
+  dt.drawPlot( "ptgOmZg", "gamma_pt/boss_mass", "", 50, 0., 2., "", "" );
 
   return 0;
 
