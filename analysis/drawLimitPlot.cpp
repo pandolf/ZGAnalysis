6d5d310b5a4f6b0a13d884392fde9b890a0f9939
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-01-28 11:41:01 +0100

    DIVIDING LIMITS BY TWO, also can pass eff and reso from comand line

--- analysis/drawLimitPlot.cpp
+++ analysis/drawLimitPlot.cpp
@@ -30,14 +30,28 @@ int main( int argc, char* argv[] ) {
   int eff = 40;
   if( argc>2 ) {
     std::string eff_str(argv[2]);
-    eff = atoi(eff_str.c_str());
+    float eff_float = atof(eff_str.c_str());
+    if( eff_float<1. ) eff_float *= 100.;
+    eff = eff_float;
+    std::cout << "-> Setting efficiency to " << eff << "%" << std::endl;
+  }
+
+  float reso_percent = 1.5;
+  if( argc>3 ) {
+    std::string reso_str(argv[3]);
+    reso_percent = atof(reso_str.c_str());
+    std::cout << "-> Setting resolution to " << reso_percent << "%" << std::endl;
   }
 
 
+  //float reso_percent = 2;
+  //float reso_percent = 5;
 
   ZGDrawTools::setStyle();
 
-  std::string dir = cfg.getEventYieldDir() + "_fit_v0_default_shapes_combination_pcorr_lumi_2.3";
+  std::string suffix = "";
+  if( reso_percent!=1.5 ) suffix = std::string(Form("_%.0fpercent", reso_percent));
+  std::string dir = cfg.getEventYieldDir() + "_fit_v0_default_shapes_combination_pcorr_lumi_2.3" + suffix;
   std::string fullPath = "../../diphotons/Analysis/macros/" + dir;
   std::string limitsFile( Form( "%s/limits_eff%d.txt", fullPath.c_str(), eff) );
 
@@ -50,6 +64,7 @@ int main( int argc, char* argv[] ) {
   ifstream ifs(limitsFile.c_str());
   std::cout << "-> Opened file: " << limitsFile << std::endl;
   int iPoint = 0;
+  float lastMass = -1;
 
   while( ifs.good() ) {
 
@@ -57,6 +72,15 @@ int main( int argc, char* argv[] ) {
     std::string s_m, s_obs, s_exp, s_exp_m1s, s_exp_m2s, s_exp_p1s, s_exp_p2s;
     ifs >> s_m >> m >> s_obs >> obs >> s_exp >> exp >> s_exp_m1s >> exp_m1s >> s_exp_m2s >> exp_m2s >> s_exp_p1s >>  exp_p1s >> s_exp_p2s >> exp_p2s;
     if( m>=1000. ) continue;
+    if( m==lastMass ) continue;
+    std::cout << "m: " << m << " obs: " << obs << " exp: " << exp << " exp_m1s: " << exp_m1s << " exp_m2s: " << exp_m2s << " exp_p1s: " << exp_p1s << " exp_p2s: " << exp_p2s << std::endl;
+
+    obs/=2.;
+    exp/=2.;
+    exp_m1s/=2.;
+    exp_m2s/=2.;
+    exp_p1s/=2.;
+    exp_p2s/=2.;
 
     gr_obs       ->SetPoint( iPoint, m, obs );
     gr_exp       ->SetPoint( iPoint, m, exp );
@@ -67,6 +91,7 @@ int main( int argc, char* argv[] ) {
     gr_exp_2sigma->SetPointError( iPoint, 0., 0., exp-exp_m2s, exp_p2s-exp );
 
     iPoint++;
+    lastMass = m;
 
   }
 
@@ -84,8 +109,9 @@ int main( int argc, char* argv[] ) {
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
   c1->cd();
 
-  TH2D* h2_axes = new TH2D("axes", "", 10, 350., 950., 10, 0., 55. );
-  h2_axes->SetYTitle( "95\% CL Upper Limit [fb]");
+  TH2D* h2_axes = new TH2D("axes", "", 10, 350., 950., 10, 0., 9. );
+  h2_axes->SetYTitle( "95\% CL UL on #sigma #times BR(A#rightarrowZ#gamma#rightarrowl^{+}l^{-}#gamma) [fb]");
+  //h2_axes->SetYTitle( "95\% CL UL [fb]");
   h2_axes->SetXTitle( "Resonance Mass [GeV]");
   h2_axes->Draw();
 
@@ -117,8 +143,8 @@ int main( int argc, char* argv[] ) {
 
   gPad->RedrawAxis();
 
-  c1->SaveAs( Form("%s/limit_eff%d.eps", cfg.getEventYieldDir().c_str(), eff) );
-  c1->SaveAs( Form("%s/limit_eff%d.pdf", cfg.getEventYieldDir().c_str(), eff) );
+  c1->SaveAs( Form("%s/limit_eff%d%s.eps", cfg.getEventYieldDir().c_str(), eff, suffix.c_str()) );
+  c1->SaveAs( Form("%s/limit_eff%d%s.pdf", cfg.getEventYieldDir().c_str(), eff, suffix.c_str()) );
 
   return 0;
 
