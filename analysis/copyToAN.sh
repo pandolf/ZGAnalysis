c756633e846dd91151ba9427ee5294f799118f9e
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-02-07 12:28:16 +0100

    added plots

---
+++ analysis/copyToAN.sh
@@ -0,0 +1,17 @@
+scp \
+    EventYields_v0/plots/nVert.pdf \
+    EventYields_v0/plots/nVert_noPU.pdf \
+    EventYields_v0/plots/mZee.pdf \
+    EventYields_v0/plots/mZmm.pdf \
+    EventYields_v0/plots/ptLept0_log.pdf \
+    EventYields_v0/plots/ptLept1_log.pdf \
+    EventYields_v0/plots/ptZ_log.pdf \
+    EventYields_v0/plots/ptGamma_log.pdf \
+    plotsGenEfficiency/genresp.pdf \
+    plotsGenEfficiency/genreso.pdf \
+    plotsGenEfficiency/geneff_all_vs_noIso.pdf \
+    genAcceptance.pdf \
+    genAcceptanceTimesEfficiency.pdf \
+    massWeightCompare_log.pdf \
+    EventYields_v0/syst*pdf \
+    lxplus.cern.ch:~/tdr2/notes/AN-16-057/trunk/figures/
