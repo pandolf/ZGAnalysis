28603bec11876365d86fcb3d7bccaa68cf028a3d
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-02-21 16:56:59 +0100

    now comparing standard with abs weights

--- analysis/compareMassShape.cpp
+++ analysis/compareMassShape.cpp
@@ -17,40 +17,40 @@ int main() {
   TTree* tree = (TTree*)file->Get("zg");
 
   float xMin = 200.;
-  float xMax = 2200.;
+  float xMax = 1700.;
+  TH1D* h1_mass_std = new TH1D("mass_std", "", 40, xMin, xMax );
   TH1D* h1_mass_pos = new TH1D("mass_pos", "", 40, xMin, xMax );
-  TH1D* h1_mass_neg = new TH1D("mass_neg", "", 40, xMin, xMax );
+  h1_mass_std->Sumw2();
   h1_mass_pos->Sumw2();
-  h1_mass_neg->Sumw2();
   
+  h1_mass_std->SetLineWidth(2);
   h1_mass_pos->SetLineWidth(2);
-  h1_mass_neg->SetLineWidth(2);
 
-  h1_mass_pos->SetLineColor(46);
-  h1_mass_neg->SetLineColor(38);
+  h1_mass_std->SetLineColor(46);
+  h1_mass_pos->SetLineColor(38);
   
-  tree->Project( "mass_pos", "boss_mass", "weight>0." );
-  tree->Project( "mass_neg", "boss_mass", "weight<0." );
+  tree->Project( "mass_std", "boss_mass", "weight" );
+  tree->Project( "mass_pos", "boss_mass", "abs(weight)" );
 
   TCanvas* c1 = new TCanvas("c1", "", 600, 600);
   c1->cd();
 
-  float yMax = h1_mass_pos->GetMaximum()/h1_mass_pos->Integral()*1.2;
+  float yMax = h1_mass_std->GetMaximum()/h1_mass_std->Integral()*1.2;
   
   TH2D* h2_axes = new TH2D("axes", "", 10, xMin, 1200., 10, 0., yMax );
   h2_axes->SetXTitle( "M(Z#gamma) [GeV]" );
   h2_axes->SetYTitle( "Normalized to Unity" );
   h2_axes->Draw("");
 
+  h1_mass_std->DrawNormalized("same");
   h1_mass_pos->DrawNormalized("same");
-  h1_mass_neg->DrawNormalized("same");
 
-  TLegend* legend = new TLegend( 0.6, 0.7, 0.9, 0.9 );
+  TLegend* legend = new TLegend( 0.5, 0.7, 0.9, 0.9 );
   legend->SetFillColor(0);
   legend->SetTextFont(42);
   legend->SetTextSize(0.038);
-  legend->AddEntry( h1_mass_pos, "w > 0", "L" );
-  legend->AddEntry( h1_mass_neg, "w < 0", "L" );
+  legend->AddEntry( h1_mass_std, "Standard weights", "L" );
+  legend->AddEntry( h1_mass_pos, "Positive weights", "L" );
   legend->Draw("same");
 
   ZGDrawTools::addLabels( c1, -1, "CMS Simulation" );
@@ -68,11 +68,11 @@ int main() {
   h2_axes_log->SetYTitle( "Normalized to Unity" );
   h2_axes_log->Draw("");
 
+  h1_mass_std->Rebin(2);
   h1_mass_pos->Rebin(2);
-  h1_mass_neg->Rebin(2);
 
+  h1_mass_std->DrawNormalized("same");
   h1_mass_pos->DrawNormalized("same");
-  h1_mass_neg->DrawNormalized("same");
 
   legend->Draw("same");
 
