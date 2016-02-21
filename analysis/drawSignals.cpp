f990e2812b03c9ee5e72f85f44a1d3c302923fc7
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-02-21 16:54:30 +0100

    added high masses

--- analysis/drawSignals.cpp
+++ analysis/drawSignals.cpp
@@ -18,7 +18,7 @@ int main( int argc, char* argv[] ) {
 
 
   if( argc==1 ) {
-    std::cout << " USAGE: ./drawZG [cfg] [lumi/shape]" << std::endl;
+    std::cout << " USAGE: ./drawSignals [cfg]" << std::endl;
     exit(1);
   }
   
@@ -31,15 +31,19 @@ int main( int argc, char* argv[] ) {
 
   TFile* file = TFile::Open(treesFile.c_str());
 
-  TTree* tree_M_450 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_450");
-  TTree* tree_M_600 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_600");
-  TTree* tree_M_750 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_750");
-  TTree* tree_M_900 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_900");
+  TTree* tree_M_450  = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_450");
+  TTree* tree_M_600  = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_600");
+  TTree* tree_M_750  = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_750");
+  TTree* tree_M_900  = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_900");
+  TTree* tree_M_1500 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_1500");
+  TTree* tree_M_2000 = (TTree*)file->Get("XZg_Spin0_ZToLL_W_0p014_M_2000");
 
   tree_M_450->SetTitle("450 GeV");
   tree_M_600->SetTitle("600 GeV");
   tree_M_750->SetTitle("750 GeV");
   tree_M_900->SetTitle("900 GeV");
+  tree_M_1500->SetTitle("1500 GeV");
+  tree_M_2000->SetTitle("2000 GeV");
 
   std::string plotsDir = cfg.getEventYieldDir() + "/plotsSignal";
   system( Form("mkdir -p %s", plotsDir.c_str()) );
@@ -49,12 +53,16 @@ int main( int argc, char* argv[] ) {
   trees_signal.push_back( tree_M_600 );
   trees_signal.push_back( tree_M_750 );
   trees_signal.push_back( tree_M_900 );
+  trees_signal.push_back( tree_M_1500 );
+  trees_signal.push_back( tree_M_2000 );
 
   
   ZGDrawTools dt( plotsDir, cfg.lumi() );
   dt.set_overlay( trees_signal );
+  dt.set_yAxisScale( 1.1 );
 
   dt.drawPlot( "sig_ptgOmZg", "gamma_pt/boss_mass", "boss_mass>200.", 25, 0., 1., "p_{T}(#gamma) / M(Z#gamma)", "" );
+  dt.drawPlot( "sig_deltaRLept", "deltaR_lept", "boss_mass>200.", 50, 0., 2., "#Delta R (leptons)", "" );
 
   return 0;
 
